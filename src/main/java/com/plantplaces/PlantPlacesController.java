2adad4bb2f15424b96eccc7b80f72007112b40b6
Justin Tracy <tracyjn@mail.uc.edu>
2022-09-22 15:10:14 -0400

    environment setup done

--- src/main/java/com/plantplaces/PlantPlacesController.java
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -1,27 +1,57 @@
 package com.plantplaces;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.plantplaces.dto.SpecimenDTO;
+import com.plantplaces.service.ISpecimenService;
 
 @Controller
 public class PlantPlacesController {
 	
-	/**
-	 * Handle the / endpoint
-	 * return
-	 *
-	 */
+	@Autowired
+	private ISpecimenService specimenServiceStub;
+	
 	@RequestMapping("/")
 	public String index() {
 		return "index";
+	}       
+	
+	@RequestMapping(value = "/start", method = RequestMethod.GET)
+	public String read(Model model) {
+		SpecimenDTO specimenDTO = specimenServiceStub.fetchById(43);
+		model.addAttribute("specimenDTO", specimenDTO);
+		return "start";
 	}
 	
-	/**
-	 * Handle the /start endpoint
-	 * return
-	 *
-	 */
-	@RequestMapping("/start")
+	@RequestMapping(value = "/start", method = RequestMethod.GET, params={"loyalty=blue"})
+	public String readBlue() { 
+		return "start";
+	}
+	
+	@RequestMapping(value = "/start", method = RequestMethod.GET, params={"loyalty=silver"})
+	public String readSilver() {
+		return "start";
+	}
+	
+	@RequestMapping(value = "/start", method = RequestMethod.GET, headers={"content-type=text/json"})
+	public ModelAndView readJSON() {
+		SpecimenDTO specimenDTO = specimenServiceStub.fetchById(43);
+		specimenDTO.setSpecimenId(83);
+		
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("start");
+		modelAndView.addObject("specimenDTO", specimenDTO);
+		
+		return modelAndView;
+	}
+	
+	@PostMapping("/start")
 	public String start() {
 		return "start";
 	}
